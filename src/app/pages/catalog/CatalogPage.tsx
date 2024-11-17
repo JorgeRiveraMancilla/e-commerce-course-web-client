a6dd6227bc3779c064c421a402a12d1a21364a8c
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-11-16 21:58:41 -0300

    refactor: use of components

--- src/app/pages/catalog/CatalogPage.tsx
+++ src/app/pages/catalog/CatalogPage.tsx
@@ -1,26 +1,16 @@
-import {
-  Box,
-  debounce,
-  Grid,
-  Pagination,
-  Paper,
-  TextField,
-  Typography,
-} from "@mui/material";
+import { Grid, Paper } from "@mui/material";
 import { useAppDispatch, useAppSelector } from "../../store/configureStore";
 import {
   setProductParams,
-  fetchFiltersAsync,
-  fetchProductsAsync,
   setPageNumber,
-  productSelectors,
 } from "../../store/slices/productSlice";
-import { ChangeEvent, useEffect, useState } from "react";
-import { Product } from "../../models/product";
 import { ProductList } from "./components/ProductList";
 import { RadioButtonGroup } from "../../components/RadioButtonGroup";
 import { CheckboxButtons } from "../../components/CheckboxButtons";
 import { Loading } from "../../components/Loading";
+import { AppPagination } from "../../components/AppPagination";
+import { useProducts } from "./hooks/useProducts";
+import { ProductSearch } from "./components/ProductSearch";
 
 const sortOptions = [
   { value: "name", label: "Alfabético" },
@@ -29,68 +19,25 @@ const sortOptions = [
 ];
 
 export const CatalogPage = () => {
-  const products: Product[] = useAppSelector(productSelectors.selectAll);
-  const {
-    productsLoaded,
-    filtersLoaded,
-    brands,
-    types,
-    productParams,
-    metaData,
-  } = useAppSelector((state) => state.product);
+  const { products, filtersLoaded, brands, types, metaData } = useProducts();
+  const { productParams } = useAppSelector((state) => state.product);
   const dispatch = useAppDispatch();
 
-  const [searchTerm, setSearchTerm] = useState(productParams.searchTerm);
-
-  useEffect(() => {
-    if (!productsLoaded) dispatch(fetchProductsAsync());
-  }, [dispatch, productsLoaded]);
-
-  useEffect(() => {
-    if (!filtersLoaded) dispatch(fetchFiltersAsync());
-  }, [dispatch, filtersLoaded]);
-
-  if (!filtersLoaded) return <Loading />;
-
-  const debouncedSearch = debounce(
-    (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-      dispatch(setProductParams({ searchTerm: event.target.value }));
-    },
-    1000
-  );
-
-  const handlePageChange = (page: number) => {
-    dispatch(setPageNumber({ pageNumber: page }));
-  };
-
-  const handleSearchProducts = (
-    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    setSearchTerm(event.target.value);
-    debouncedSearch(event);
-  };
+  if (!filtersLoaded) return <Loading message="Cargando productos..." />;
 
   return (
     <Grid container columnSpacing={4}>
       <Grid item xs={3}>
         <Paper sx={{ mb: 2 }}>
-          <TextField
-            label="Buscar productos"
-            variant="outlined"
-            fullWidth
-            value={searchTerm || ""}
-            onChange={(
-              event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-            ) => handleSearchProducts(event)}
-          />
+          <ProductSearch />
         </Paper>
 
         <Paper sx={{ p: 2, mb: 2 }}>
           <RadioButtonGroup
             selectedValue={productParams.orderBy}
             options={sortOptions}
-            onChange={(event) =>
-              dispatch(setProductParams({ orderBy: event.target.value }))
+            onChange={(e) =>
+              dispatch(setProductParams({ orderBy: e.target.value }))
             }
           />
         </Paper>
@@ -124,28 +71,12 @@ export const CatalogPage = () => {
 
       <Grid item xs={9} sx={{ mb: 2 }}>
         {metaData && (
-          <Box
-            display="flex"
-            justifyContent="space-between"
-            alignItems="center"
-            sx={{ marginBottom: 3 }}
-          >
-            <Typography variant="body1">
-              Mostrando {(metaData.currentPage - 1) * metaData.pageSize + 1}-
-              {metaData.currentPage * metaData.pageSize > metaData.totalCount
-                ? metaData.totalCount
-                : metaData.currentPage * metaData.pageSize}{" "}
-              de {metaData.totalCount} ítems
-            </Typography>
-
-            <Pagination
-              color="secondary"
-              size="large"
-              count={metaData.totalPages}
-              page={metaData.currentPage}
-              onChange={(_e, page) => handlePageChange(page)}
-            />
-          </Box>
+          <AppPagination
+            metaData={metaData}
+            onPageChange={(page: number) =>
+              dispatch(setPageNumber({ pageNumber: page }))
+            }
+          />
         )}
       </Grid>
     </Grid>

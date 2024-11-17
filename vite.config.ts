fa7af4fa72ef58fbee734026ae17ab2038fb2bf8
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-11-16 23:30:37 -0300

    build: update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import vercel from "vite-plugin-vercel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vercel()],
+  plugins: [react()],
   server: {
     port: process.env.PORT as unknown as number,
   },

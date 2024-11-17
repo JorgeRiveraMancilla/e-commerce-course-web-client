893a579b3530f5e7c8454a80ca37c4944aa34bcd
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-11-16 21:56:54 -0300

    refactor: add messages

--- src/app/pages/checkout/validations/addressFormValidation.ts
+++ src/app/pages/checkout/validations/addressFormValidation.ts
@@ -2,16 +2,16 @@ import * as yup from "yup";
 
 export const validationSchema = [
   yup.object({
-    fullName: yup.string().required("Full name is required"),
-    address1: yup.string().required("Addres line 1 is required"),
-    address2: yup.string().required(),
-    city: yup.string().required(),
-    state: yup.string().required(),
-    postalCode: yup.string().required(),
-    country: yup.string().required(),
+    fullName: yup.string().required("El nombre completo es requerido"),
+    address1: yup.string().required("La línea 1 de la dirección es requerida"),
+    address2: yup.string().required("La línea 2 de la dirección es requerida"),
+    city: yup.string().required("La ciudad es requerida"),
+    state: yup.string().required("El estado es requerido"),
+    postalCode: yup.string().required("El código postal es requerido"),
+    country: yup.string().required("El país es requerido"),
   }),
   yup.object(),
   yup.object({
-    nameOnCard: yup.string().required(),
+    nameOnCard: yup.string().required("El nombre en la tarjeta es requerido"),
   }),
 ];
